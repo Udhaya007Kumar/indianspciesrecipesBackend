9bb9b166104788f18311ed5e0bf5946780ffc9fa
Udhaya007Kumar <udhaya93kumar@gmail.com>
2024-11-23 00:32:50 +0530

    ss

--- Utils/generateToken.js
+++ Utils/generateToken.js
@@ -5,15 +5,15 @@ dotenv.config();
 
 
 
-const generateToken = (userId,res) => {
+const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
-    
+
     res.cookie("jwt", token, {
-        httpOnly: true,
-        maxAge: 15 * 24 * 60 * 1000,
-        sameSite:"None",
-        secure: process.env.NODE_ENV !== "production"
+        httpOnly: true, // Prevents access to cookie via client-side scripts
+        maxAge: 15 * 24 * 60 * 60 * 1000, // Corrected maxAge (15 days in milliseconds)
+        sameSite: "None", // Set to "None" for cross-origin requests
+        secure: process.env.NODE_ENV === "production", // Secure only in production
     });
-}
+};
 
 export default generateToken;
